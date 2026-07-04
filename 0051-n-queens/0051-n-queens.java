83cd897b455ccb6c6c8b4b50f0f1f243c1b8b5bd
Shivran kumari <shivrankumari79@gmail.com>
2026-07-04 13:37:13 +0530

    Time: 2 ms (83.45%), Space: 46.8 MB (28.16%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        backtrack(0, board, result);
+
+        return result;
+    }
+
+    private void backtrack(int row, char[][] board, List<List<String>> result) {
+        if (row == board.length) {
+            result.add(construct(board));
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                backtrack(row + 1, board, result);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col) {
+        // Check same column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> temp = new ArrayList<>();
+        for (char[] row : board) {
+            temp.add(new String(row));
+        }
+        return temp;
+    }
+}
