ff3e3108ad50b8ac0a0f8b406ed64ea19e90c1f6
Shivran kumari <shivrankumari79@gmail.com>
2026-07-28 20:08:32 +0530

    Time: 2 ms (61.49%), Space: 42.1 MB (43.16%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,70 @@
+class Solution {
+
+    int count = 0;
+
+    public int totalNQueens(int n) {
+
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        backtrack(board, 0, n);
+
+        return count;
+    }
+
+    private void backtrack(char[][] board, int row, int n) {
+
+        
+        if (row == n) {
+            count++;
+            return;
+        }
+
+        
+        for (int col = 0; col < n; col++) {
+
+            if (isSafe(board, row, col, n)) {
+
+                
+                board[row][col] = 'Q';
+
+                
+                backtrack(board, row + 1, n);
+
+                
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col, int n) {
+
+        
+        for (int i = 0; i < row; i++) {
+
+            if (board[i][col] == 'Q')
+                return false;
+        }
+        for (int i = row - 1, j = col - 1;
+             i >= 0 && j >= 0;
+             i--, j--) {
+
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        
+        for (int i = row - 1, j = col + 1;
+             i >= 0 && j < n;
+             i--, j++) {
+
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+}
